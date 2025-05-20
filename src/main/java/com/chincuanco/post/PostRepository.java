224c383b5a3ab0948fc8206d674199309c59cca5
Lance131 <Lpchincuanco.student@ua.edu.ph>
2025-05-20 12:24:41 +0800

    first commit

---
+++ src/main/java/com/chincuanco/post/PostRepository.java
@@ -0,0 +1,8 @@
+package com.chincuanco.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
